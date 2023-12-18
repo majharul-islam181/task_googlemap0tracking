137553d7483eb06b968da630973047e5771e4b1d
majharul181 <majharul.islam03@northsouth.edu>
2023-12-19 01:14:44 +0600

    added google api key for ios

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const String google_api_key = "";
+const String google_api_key = "AIzaSyD3NXwgTIq76erB9Nbf8AqdcKje83_FrVE";
 
 const Color primaryColor = Color(0xFF7861FF);
 
