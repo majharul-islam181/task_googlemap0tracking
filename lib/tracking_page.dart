3eac311fb71abba5c69653046c69da246335d70b
majharul181 <majharul.islam03@northsouth.edu>
2023-12-19 00:31:19 +0600

    sample tracking page

--- lib/tracking_page.dart
+++ lib/tracking_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class TrackingPage extends StatefulWidget {
   const TrackingPage({super.key});
@@ -8,8 +9,19 @@ class TrackingPage extends StatefulWidget {
 }
 
 class _TrackingPageState extends State<TrackingPage> {
+  static const LatLng staringLocation = LatLng(37.33500926, -122.03272188);
+  static const LatLng destinationLocation = LatLng(37.33429383, -122.06600055);
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Traking position',
+        style: TextStyle(color: Colors.black, fontSize: 16),),
+        
+      ),
+      body: const Center(
+        child: Text('Task: GoogleMap tracking......'),
+      ),
+    );
   }
 }
